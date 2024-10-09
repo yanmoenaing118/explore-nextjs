1a4384071662aa6f6f5a558e435f87ae8acbac36
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-10-09 21:53:36 +0630

    reset value

--- app/components/form/Form.tsx
+++ app/components/form/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Control, Controller, useForm } from "react-hook-form";
+import { Control, Controller, useFieldArray, useForm } from "react-hook-form";
 import * as yup from "yup";
 
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -21,38 +21,51 @@ import { ErrorMessage } from "@hookform/error-message";
 
 */
 
+const schema = yup.object().shape({
+  links: yup.array().of(
+    yup.object({
+      id: yup.string(),
+      name: yup.string().nullable(),
+      //   platform: yup.string().required().nullable(),
+      platform: yup.lazy(() =>
+        yup.string().when("destination", {
+          is: (val: string) => val && val !== "",
+          then: (schema) => schema.required(),
+          otherwise: (schema) => schema.optional().nullable(),
+        })
+      ),
+      destination: yup.lazy(() =>
+        yup.string().when("platform", {
+          is: (val: string | null) => val == null,
+          then: (schema) => schema.optional(),
+          otherwise: (schema) => schema.required(),
+        })
+      ),
+      private: yup.boolean(),
+    })
+  ),
+});
+
 // const schema = yup.object().shape({
-//   links: yup.array().of(
-//     yup.object({
-//       id: yup.string(),
-//       name: yup.string().required().nullable(),
-//       platform: yup.string().required(),
-//       destination: yup.string().required(),
-//       private: yup.boolean(),
+//   id: yup.string(),
+//   name: yup.string().nullable(),
+//   //   platform: yup.string().required().nullable(),
+//   platform: yup.lazy(() =>
+//     yup.string().when("destination", {
+//       is: (val: string) => val && val !== "",
+//       then: (schema) => schema.required(),
+//       otherwise: (schema) => schema.optional().nullable(),
 //     })
 //   ),
+//   destination: yup.lazy(() =>
+//     yup.string().when("platform", {
+//       is: (val: string | null) => val == null,
+//       then: (schema) => schema.optional(),
+//       otherwise: (schema) => schema.required(),
+//     })
+//   ),
+//   private: yup.boolean(),
 // });
-
-const schema = yup.object().shape({
-  id: yup.string(),
-  name: yup.string().nullable(),
-  //   platform: yup.string().required().nullable(),
-  platform: yup.lazy(() =>
-    yup.string().when("destination", {
-      is: (val: string) => val && val !== "",
-      then: (schema) => schema.required(),
-      otherwise: (schema) => schema.optional().nullable(),
-    })
-  ),
-  destination: yup.lazy(() =>
-    yup.string().when("platform", {
-      is: (val: string | null) => val == null,
-      then: (schema) => schema.optional(),
-      otherwise: (schema) => schema.required(),
-    })
-  ),
-  private: yup.boolean(),
-});
 
 /**
  * if platform is null
@@ -63,6 +76,13 @@ const schema = yup.object().shape({
  */
 
 type LinksType = yup.InferType<typeof schema>;
+
+const emptyLink = {
+  name: "",
+  platform: null as any,
+  destination: "",
+  private: false,
+};
 
 export default function Form() {
   const {
@@ -75,35 +95,71 @@ export default function Form() {
     setValue,
   } = useForm<LinksType>({
     resolver: yupResolver(schema),
+    defaultValues: {
+      links: [
+        {
+          name: "",
+          platform: null as any,
+          destination: "",
+          private: false,
+        },
+      ],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control: control,
+    name: "links",
   });
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
   });
 
-  const platform = watch("platform");
+  //   const platform = watch("platform");
 
-  useEffect(() => {
-    setValue("destination", "");
-  }, [platform]);
+  //   useEffect(() => {
+  //     setValue("destination", "");
+  //   }, [platform]);
+
+  const addLink = () => {
+    append(emptyLink);
+  };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <div>
-          <div>
-            <input
-              className="border block w-full mb-3 p-3"
-              type="text"
-              {...register("destination")}
-            />
-          </div>
-          <div>
-            <Dropdown name="platform" control={control} errors={errors} />
-          </div>
-        </div>
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              <div>
+                <input
+                  key={field.id}
+                  {...register(`links.${index}.destination`)}
+                  className="border block w-full mb-3 p-3"
+                  type="text"
+                />
+              </div>
+              <div>
+                <Dropdown
+                  name={`links.${index}.platform`}
+                  control={control}
+                  errors={errors}
+                  index={index}
+                  setValue={setValue}
+                />
+              </div>
+              <div className="flex gap-3">
+                <span className="p-3 shadow-sm" onClick={addLink}>
+                  Add
+                </span>
+                <span className="p-3 shadow-sm">Remove</span>
+              </div>
+            </div>
+          );
+        })}
 
-        <div>
+        <div className="flex justify-between">
           <button type="submit" className="border p-3 shadow-sm rounded">
             submit
           </button>
@@ -119,10 +175,14 @@ const Dropdown = ({
   name,
   control,
   errors,
+  index,
+  setValue,
 }: {
   name: string;
   control: Control<any>;
   errors: any;
+  index: number;
+  setValue: any;
 }) => {
   return (
     <Controller
@@ -135,31 +195,46 @@ const Dropdown = ({
               selected={field.value === null}
               text="Select"
               value={null}
-              onClick={field.onChange}
+              onClick={(v) => {
+                setValue(`links.${index}.destination`, "");
+                field.onChange(v);
+              }}
             />
             <Item
               selected={field.value === "Facebook"}
               text="Facebook"
               value={"Facebook"}
-              onClick={field.onChange}
+              onClick={(v) => {
+                setValue(`links.${index}.destination`, "");
+                field.onChange(v);
+              }}
             />
             <Item
               selected={field.value === "Google"}
               text="Google"
               value={"Google"}
-              onClick={field.onChange}
+              onClick={(v) => {
+                setValue(`links.${index}.destination`, "");
+                field.onChange(v);
+              }}
             />
             <Item
               selected={field.value === "Youtube"}
               text="Youtube"
               value={"Youtube"}
-              onClick={field.onChange}
+              onClick={(v) => {
+                setValue(`links.${index}.destination`, "");
+                field.onChange(v);
+              }}
             />
             <Item
               selected={field.value === "LinkedIn"}
               text="LinkedIn"
               value={"LinkedIn"}
-              onClick={field.onChange}
+              onClick={(v) => {
+                setValue(`links.${index}.destination`, "");
+                field.onChange(v);
+              }}
             />
           </div>
           <ErrorMessage
